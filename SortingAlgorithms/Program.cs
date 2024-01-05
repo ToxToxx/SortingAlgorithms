27ba819ab551b2c5c25f9a83b8a407db784f17c2
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2024-01-05 15:41:24 +0300

    learn merge sort

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -16,6 +16,8 @@
             //BubbleSort(numbersArray);
             //SelectionSort(numbersArray);           
             //WriteQuickSort(numbersArray);
+            //WriteBottomUpMergeSort(numbersArray);
+            WriteUpDownMergeSort(numbersArray);
 
         }
 
@@ -86,6 +88,8 @@
             }
             return numbers;
         }
+
+
         static void SwapQuickSort(ref int firstElement, ref int secondElement)
         {
             var temp = firstElement;
@@ -93,51 +97,179 @@
             secondElement = temp;
         }
 
-        static int PartitionQuickSort(int[] array, int minIndex, int maxIndex)
+        static int PartitionQuickSort(int[] numbers, int minIndex, int maxIndex)
         {
             var pivot = minIndex - 1;
             for (var i = minIndex; i < maxIndex; i++)
             {
-                if (array[i] < array[maxIndex])
+                if (numbers[i] < numbers[maxIndex])
                 {
                     pivot++;
-                    SwapQuickSort(ref array[pivot], ref array[i]);
+                    SwapQuickSort(ref numbers[pivot], ref numbers[i]);
                 }
             }
 
             pivot++;
-            SwapQuickSort(ref array[pivot], ref array[maxIndex]);
+            SwapQuickSort(ref numbers[pivot], ref numbers[maxIndex]);
             return pivot;
         }
 
-        static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        static int[] QuickSort(int[] numbers, int minIndex, int maxIndex)
         {
             if (minIndex >= maxIndex)
             {
-                return array;
+                return numbers;
             }
 
-            var pivotIndex = PartitionQuickSort(array, minIndex, maxIndex);
-            QuickSort(array, minIndex, pivotIndex - 1);
-            QuickSort(array, pivotIndex + 1, maxIndex);
+            var pivotIndex = PartitionQuickSort(numbers, minIndex, maxIndex);
+            QuickSort(numbers, minIndex, pivotIndex - 1);
+            QuickSort(numbers, pivotIndex + 1, maxIndex);
 
-            return array;
+            return numbers;
         }
 
-        static int[] QuickSort(int[] array)
+        static int[] QuickSort(int[] numbers)
         {
-            return QuickSort(array, 0, array.Length - 1);
+            return QuickSort(numbers, 0, numbers.Length - 1);
         }
 
-        static void WriteQuickSort(int[] array)
+        static void WriteQuickSort(int[] numbers)
         {
             Console.WriteLine();
             Console.WriteLine("Quick Sort: ");
-            int[] quickSortOutput = QuickSort(array);
+            int[] quickSortOutput = QuickSort(numbers);
             foreach (var num in quickSortOutput)
             {
                 Console.Write(num + " ");
             }
         }
+
+
+        static void BottomUpMergeSort(int[] numbers)
+        {
+            if (numbers.Length > 1)
+            {
+                int mid = numbers.Length / 2;
+                int[] leftHalf = new int[mid];
+                int[] rightHalf = new int[numbers.Length - mid];
+
+                Array.Copy(numbers, 0, leftHalf, 0, mid);
+                Array.Copy(numbers, mid, rightHalf, 0, numbers.Length - mid);
+
+                BottomUpMergeSort(leftHalf);
+                BottomUpMergeSort(rightHalf);
+
+                int i = 0, j = 0, k = 0;
+                while (i < leftHalf.Length && j < rightHalf.Length)
+                {
+                    if (leftHalf[i] < rightHalf[j])
+                    {
+                        numbers[k] = leftHalf[i];
+                        i++;
+                    }
+                    else
+                    {
+                        numbers[k] = rightHalf[j];
+                        j++;
+                    }
+                    k++;
+                }
+
+                while (i < leftHalf.Length)
+                {
+                    numbers[k] = leftHalf[i];
+                    i++;
+                    k++;
+                }
+
+                while (j < rightHalf.Length)
+                {
+                    numbers[k] = rightHalf[j];
+                    j++;
+                    k++;
+                }
+            }
+        }
+        static void WriteBottomUpMergeSort(int[] numbers)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Bottom Up Merge Sort: ");
+            BottomUpMergeSort(numbers);
+            foreach (var num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+        }
+
+
+        static void Merge(int[] numbers, int lowIndex, int middleIndex, int highIndex)
+        {
+            var left = lowIndex;
+            var right = middleIndex + 1;
+            var tempArray = new int[highIndex - lowIndex + 1];
+            var index = 0;
+
+            while ((left <= middleIndex) && (right <= highIndex))
+            {
+                if (numbers[left] < numbers[right])
+                {
+                    tempArray[index] = numbers[left];
+                    left++;
+                }
+                else
+                {
+                    tempArray[index] = numbers[right];
+                    right++;
+                }
+
+                index++;
+            }
+
+            for (var i = left; i <= middleIndex; i++)
+            {
+                tempArray[index] = numbers[i];
+                index++;
+            }
+
+            for (var i = right; i <= highIndex; i++)
+            {
+                tempArray[index] = numbers[i];
+                index++;
+            }
+
+            for (var i = 0; i < tempArray.Length; i++)
+            {
+                numbers[lowIndex + i] = tempArray[i];
+            }
+        }
+
+        static int[] UpDownMergeSort(int[] numbers, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                var middleIndex = (lowIndex + highIndex) / 2;
+                UpDownMergeSort(numbers, lowIndex, middleIndex);
+                UpDownMergeSort(numbers, middleIndex + 1, highIndex);
+                Merge(numbers, lowIndex, middleIndex, highIndex);
+            }
+
+            return numbers;
+        }
+
+        static int[] UpDownMergeSort(int[] numbers)
+        {
+            return UpDownMergeSort(numbers, 0, numbers.Length - 1);
+        }
+        static void WriteUpDownMergeSort(int[] numbers)
+        {
+            Console.WriteLine();
+            Console.WriteLine("UpDownMerge Sort: ");
+            int[] mergeSortOutput = UpDownMergeSort(numbers);
+            foreach (var num in mergeSortOutput)
+            {
+                Console.Write(num + " ");
+            }
+        }
+
     }
 }
