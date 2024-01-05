ceb8bfbfd371dc2a5f72382e585c716d55259b7e
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2024-01-05 14:19:28 +0300

    Initial commit

---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SortingAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArray = { 1, 5, 2, 40, 23, 96, 28 };
+
+            Console.WriteLine("Original array: ");
+            foreach (var num in numbersArray)
+            {
+                Console.Write(num + " ");
+            }
+
+            InsertionSort(numbersArray);
+
+
+        }
+
+        static int[] InsertionSort(int[] numbers)
+        {
+            for (int j = 2; j < numbers.Length; j++)
+            {
+                int key = numbers[j];
+                int i = j - 1;
+                while(i >= 0 && numbers[i] > key)
+                {
+                    numbers[i + 1] = numbers[i];
+                    i -= 1;
+                }
+                numbers[i + 1] = key;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Insertion sort: ");
+            foreach (var num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            return numbers;
+        }
+    }
+}
